1368f43925c221de231134aca1ea3ef14507c482
CardenasGalarza <alex10estadistica@gmail.com>
2022-10-10 23:45:08 -0500

    affe

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ email = \"alice.dt.control@gmail.com\"\n\
 
 echo "\
 [theme]\n\
-base='light'\n\
-primaryColor='#f63366'\n\
-backgroundColor='#355c7d'\n\
-secondaryBackgroundColor = '#d6d5d5'\n\
+base='dark'\n\
+primaryColor='#02fbde'\n\
+backgroundColor='#00022b'\n\
+secondaryBackgroundColor = '#043562'\n\
 textColor='#000000'\n\
 font = 'sans serif'\n\
 
@@ -19,3 +19,5 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+
