74ce40e28da69b97d6f2223bb866cb6b6628f6b7
CardenasGalarza <alex10estadistica@gmail.com>
2022-10-13 18:38:04 -0500

    hiqhiwhidh

--- app.py
+++ app.py
@@ -393,7 +393,19 @@ if authentication_status:
     cursor.close()
     cnxn.close()
     
-    try:              ## borrar nombres de la pagina
+    try:
+        
+        ## botones en general
+        primaryColor = st.get_option("theme.primaryColor")
+        s = f"""
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Atma:wght@600&display=swap');
+        div.stButton > button:first-child {{ border: 5px solid {primaryColor}; border-radius:20px 20px 20px 20px; }}
+        <style>
+        """
+        st.markdown(s, unsafe_allow_html=True)
+
+        ## borrar nombres de la pagina
         hide_streamlit_style = """
                     <style>
                     #MainMenu {visibility: hidden;}
