07df55e76bc86e35d99860eaf8de61332364c280
CardenasGalarza <alex10estadistica@gmail.com>
2022-10-23 12:07:16 -0500

    efrf

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ if authentication_status:
                     #import pyautogui
                     #pyautogui.hotkey("ctrl","F5")
                     #st.experimental_singleton.clear()
-                    time.sleep(0.1)
+                    #time.sleep(0.1)
                     st.experimental_rerun()
 
                     #
